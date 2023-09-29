2b6c224803842d413898e3dad0a1fc3ce5be3532
SamAkinbile <>
2023-09-29 11:04:07 +0000

    JS added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,40 @@
-const questions = [
-    {
-        question: "What is the capital of France?",
-        options: ["London", "Berlin", "Paris"],
-        correctAnswer: "Paris"
-    },
-    {
-        question: "Which planet is known as the Red Planet?",
-        options: ["Venus", "Mars", "Jupiter"],
-        correctAnswer: "Mars"
-    },
-    // Add more questions here
-];
+let right1 = document.getElementById("right1");
+let right2 = document.getElementById("right2");
+let right3 = document.getElementById("right3");
+let audio = document.getElementById("myAudio");
+var answer = 0;
 
-let currentQuestionIndex = 34;
-let score = 0;
-
-function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    document.getElementById("score").textContent = score;
-    showQuestion(currentQuestionIndex);
-    document.getElementById("start-button").style.display = "none";
-    document.getElementById("feedback").textContent = "";
+function generate_logic() {
+    var fig1 = Math.floor(Math.random() * 13);
+    var fig2 = Math.floor(Math.random() * 13);
+    var fakeAnswe1 = Math.floor(Math.random() * 13);
+    var fakeAnswer2 = Math.floor(Math.random() * 13);
+    var allAnswer;
 }
 
-function showQuestion(index) {
-    const questionElement = document.getElementById("question");
-    questionElement.textContent = questions[index].question;
-
-    const options = document.querySelectorAll(".option");
-    for (let i = 0; i < options.length; i++) {
-        options[i].textContent = questions[index].options[i];
+right1.addEventListener("click", function () {
+    if (right1.innerHTML == answer) {
+        generate_logic();
     }
-}
-
-function checkAnswer(selectedOption) {
-    const selectedAnswer = selectedOption.textContent;
-    const correctAnswer = questions[currentQuestionIndex].correctAnswer;
-
-    if (selectedAnswer === correctAnswer) {
-        score++;
-        document.getElementById("score").textContent = score;
-        document.getElementById("feedback").textContent = "Correct!";
-    } else {
-        document.getElementById("feedback").textContent = "Incorrect. Try again!";
+    else {
+        audio.play();
     }
+});
 
-    currentQuestionIndex++;
-
-    if (currentQuestionIndex < questions.length) {
-        showQuestion(currentQuestionIndex);
-    } else {
-        endQuiz();
+right2.addEventListener("click", function () {
+    if (right2.innerHTML == answer) {
+        generate_logic();
     }
-}
+    else {
+        audio.play();
+    }
+});
 
-function endQuiz() {
-    document.getElementById("feedback").textContent = `Quiz completed. Your score: ${score}/${questions.length}`;
-    document.getElementById("start-button").style.display = "block";
-}
-
-document.getElementById("start-button").addEventListener("click", startQuiz);
-
-// You can add more features like a username input, images, and more questions.
+right3.addEventListener("click", function () {
+    if (right3.innerHTML == answer) {
+        generate_logic();
+    }
+    else {
+        audio.play();
+    }
+});
