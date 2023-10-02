efdd67bbbfa96c2df20a1e3202c60e9fd9b04d9d
SamAkinbile <>
2023-10-02 14:56:00 +0000

    js

--- assets/js/multiplication.js
+++ assets/js/multiplication.js
@@ -3,23 +3,19 @@ let right2 = document.getElementById("right2");
 let right3 = document.getElementById("right3");
 let audio = document.getElementById("myAudio");
 let answer = 0;
+let score = 0;
+let timeLeft = 10;
 
 function generate_logic() {
     let fig1 = Math.floor(Math.random() * 13);
     let fig3 = Math.floor(Math.random() * 13);
-    let dummyAnswer1 = Math.floor(Math.random() * 50);
-    let dummyAnswer2 = Math.floor(Math.random() * 50);
-    allAnswers = [],
-        switchAnswers = [];
-
-
     answer = fig1 * fig3;
 
     document.getElementById("fig1").innerHTML = fig1;
     document.getElementById("fig3").innerHTML = fig3;
+    document.getElementById("operation").innerHTML = "x";
 
-
-    let allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+    let allAnswers = [answer, Math.floor(Math.random() * 169), Math.floor(Math.random() * 169)];
 
     // Shuffle the answer choices
     let switchAnswers = shuffleArray(allAnswers);
@@ -37,28 +33,61 @@ function shuffleArray(array) {
     return array;
 }
 
+function updateScore() {
+    score++;
+    document.getElementById("score").innerHTML = score;
+}
+
+function updateTime() {
+    timeLeft--;
+    document.getElementById("timer").innerHTML = timeLeft;
+
+    if (timeLeft === 0) {
+        clearInterval(timerId);
+        alert(`Time's up! Your final score is ${score}.`);
+        location.reload();
+    }
+}
+
+generate_logic();
+
+let timerId = setInterval(updateTime, 1000);
+
 right1.addEventListener("click", function () {
     if (right1.innerHTML == answer) {
         generate_logic();
+        updateScore();
+        timeLeft += 2;
+        document.getElementById("timer").innerHTML = timeLeft;
     } else {
         audio.play();
+        timeLeft--;
+        document.getElementById("timer").innerHTML = timeLeft;
     }
 });
 
 right2.addEventListener("click", function () {
     if (right2.innerHTML == answer) {
         generate_logic();
+        updateScore();
+        timeLeft += 2;
+        document.getElementById("timer").innerHTML = timeLeft;
     } else {
         audio.play();
+        timeLeft--;
+        document.getElementById("timer").innerHTML = timeLeft;
     }
 });
 
 right3.addEventListener("click", function () {
     if (right3.innerHTML == answer) {
         generate_logic();
+        updateScore();
+        timeLeft += 2;
+        document.getElementById("timer").innerHTML = timeLeft;
     } else {
         audio.play();
+        timeLeft--;
+        document.getElementById("timer").innerHTML = timeLeft;
     }
 });
-
-generate_logic();
